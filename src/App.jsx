70735fc4b08241726cbbab2b53ec95caf9764763
Gareth Dawson <gwdawson.work@gmail.com>
2022-06-08 16:31:31 +0100

    feat: setup /login, /signup, /profile routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import logo from './assets/logo.svg';
 import './styles/App.css';
+import { useState } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
+  const [theme, setTheme] = useState('light');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div className={`App app-${theme}`}>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<h1>LOGIN</h1>} />
+        <Route path="/signup" element={<h1>SIGNUP</h1>} />
+        <Route path="/profile" element={<h1>PROFILE</h1>} />
+        <Route path="/*" element={<h1>ERROR</h1>} />
+      </Routes>
     </div>
   );
 }
