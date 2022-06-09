fce06f4d16c45a5587a5e4811adf8526359d0b94
Gareth Dawson <gwdawson.work@gmail.com>
2022-06-09 11:17:03 +0100

    feat: add database request functions

---
+++ src/utils/connection.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+
+const server = axios.create({
+  baseURL: 'http://localhost:9000',
+});
+
+export const getUserByUsername = async (userName) => {
+  const { data } = await server.get(`/username/${userName}`);
+  return data;
+};
+
+export const getUserById = async (userId) => {
+  const { data } = await server.get(`/userid/${userId}`);
+  return data;
+};
+
+export const postUser = async (userID, userName, userPassword) => {
+  const { data } = await server.post('/user', {
+    user_id: userID,
+    user_name: userName,
+    user_password: userPassword,
+  });
+  return data;
+};
