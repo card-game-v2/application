837e1326fc53da5c22cc61322c95f0040fcad9c7
Gareth Dawson <gwdawson.work@gmail.com>
2022-06-09 16:51:04 +0100

    chore: update variable names

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -4,22 +4,17 @@ const server = axios.create({
   baseURL: 'http://localhost:9000',
 });
 
-export const getUserByUsername = async (userName) => {
-  const { data } = await server.get(`/users/u/${userName}`);
+export const getUserByUsername = async (user_name) => {
+  const { data } = await server.get(`/users/u/${user_name}`);
   return data;
 };
 
-export const getUserById = async (userId) => {
-  const { data } = await server.get(`/users/i/${userId}`);
+export const getUserById = async (user_id) => {
+  const { data } = await server.get(`/users/i/${user_id}`);
   return data;
 };
 
-export const postUser = async (userID, userName, userPassword, userAvatarURL) => {
-  const { data } = await server.post('/users', {
-    user_id: userID,
-    user_name: userName,
-    user_password: userPassword,
-    user_avatar_url: userAvatarURL,
-  });
+export const postUser = async (user_id, user_name, user_password, user_avatar_url) => {
+  const { data } = await server.post('/users', { user_id, user_name, user_password, user_avatar_url });
   return data;
 };
