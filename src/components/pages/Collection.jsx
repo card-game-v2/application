cd1dc3eb49f8e41846637a85dfedcb6c490a8e8b
Gareth Dawson <gwdawson.work@gmail.com>
2022-06-10 17:58:48 +0100

    feat: add loading state to collection

--- src/components/pages/Collection.jsx
+++ src/components/pages/Collection.jsx
@@ -1,19 +1,26 @@
 import './Collection.css';
 import React, { useState, useEffect } from 'react';
 import Tilt from 'react-parallax-tilt';
+import Loading from '../loading/Loading';
 
 import { getUsercardsById } from '../../utils/connection';
 
 const Profile = ({ theme, user }) => {
+  const [loading, setLoading] = useState(true);
   const [usercards, setUsercards] = useState([]);
 
   useEffect(() => {
     const makeAsync = async () => {
       const { data } = await getUsercardsById(user.user_id);
       setUsercards(data.data);
+      setTimeout(() => {
+        setLoading(false);
+      }, 1400);
     };
     makeAsync();
   }, [user.user_id]);
+
+  if (loading) return <Loading theme={theme} />;
 
   return (
     <section className={`collection collection-${theme}`}>
