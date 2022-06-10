6a603ae7a118fe8413615cda3805185301a105b5
Gareth Dawson <gwdawson.work@gmail.com>
2022-06-10 11:33:25 +0100

    refactor: clean up utility functions

--- src/utils/function.js
+++ src/utils/function.js
@@ -1,18 +1,14 @@
-import { getUserById, postUsercards, incrementCardIssue } from './connection';
+import { getUserById } from './connection';
 
-export const createID = async () => {
+export const createUniqueUserID = async () => {
   const array = [];
+
   for (let i = 0; array.length < 16; i++) {
     const number = Math.floor(Math.random() * 10);
     if (number !== 0) array.push(number);
   }
-  const id = parseInt(array.join(''));
-  const { message } = await getUserById(id);
-  if (message === 'User not found') return id;
-  return createID();
-};
 
-export const spawnRandomCard = async (user_id, card_id) => {
-  const { data } = await incrementCardIssue(card_id);
-  await postUsercards(user_id, card_id, data.card_issue, 'H', 0);
+  const { data } = await getUserById(parseInt(array.join('')));
+  if (data.message === 'error') return parseInt(array.join(''));
+  return createUniqueUserID();
 };
