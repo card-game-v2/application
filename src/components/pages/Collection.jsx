534faa0e1ebd68e4c0fa5264f1d9e81a72d2c09c
Gareth Dawson <gwdawson.work@gmail.com>
2022-06-10 13:33:25 +0100

    feat: render usercards

--- src/components/pages/Collection.jsx
+++ src/components/pages/Collection.jsx
@@ -1,20 +1,31 @@
 import './Collection.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Tilt from 'react-parallax-tilt';
 
-// import { getUsercardsById } from '../../utils/connection';
+import { getUsercardsById } from '../../utils/connection';
 
-const Profile = ({ theme, user, spawn }) => {
+const Profile = ({ theme, user }) => {
   const [usercards, setUsercards] = useState([]);
+
+  useEffect(() => {
+    const makeAsync = async () => {
+      const { data } = await getUsercardsById(user.user_id);
+      setUsercards(data.data);
+    };
+    makeAsync();
+  }, [user.user_id]);
 
   return (
     <section className={`collection collection-${theme}`}>
       <div className={`collection-cards collection-cards-${theme}`}>
-        {usercards.map((usercard) => (
-          <Tilt key={`${usercard.usercards_id}${usercard.usercards_issue}`}>
-            <img className={`collection-card`} src={usercard.card_image_url} alt="card" />
-          </Tilt>
-        ))}
+        {usercards.length === 0 && <h1>YOUR COLLECTION IS EMPTY</h1>}
+        {usercards.map((usercard) => {
+          return (
+            <Tilt key={`${usercard.card_id}${usercard.issue}`}>
+              <img className={`collection-card`} src={usercard.image_url} alt="card" />
+            </Tilt>
+          );
+        })}
       </div>
     </section>
   );
