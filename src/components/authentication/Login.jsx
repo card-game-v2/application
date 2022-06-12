5a5b0995d3d2610220a5301ade4957e04cad3392
Gareth Dawson <gwdawson.work@gmail.com>
2022-06-12 14:56:12 +0100

    feat: compare password to encrypted password

--- src/components/authentication/Login.jsx
+++ src/components/authentication/Login.jsx
@@ -2,6 +2,7 @@ import './Authentication.css';
 import React from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import bcrypt from 'bcryptjs';
 
 import { getUserByUsername } from '../../utils/connection';
 
@@ -16,7 +17,8 @@ const Login = ({ theme, setAuth, setUser }) => {
     if (username.length < 1 || password.length < 1) return toast.error('Invalid username or password');
 
     const { data } = await getUserByUsername(e.target[0].value);
-    if (data.message === 'error' || password !== data.data.password) return toast.error('Invalid username or password');
+    if (data.message === 'error' || !bcrypt.compareSync(password, data.data.password))
+      return toast.error('Invalid username or password');
 
     toast.success('Login successful');
     setAuth(true);
