bdf31a8921661b9e1e2024d77264ebd4eb8e1f59
Gareth Dawson <gwdawson.work@gmail.com>
2022-06-14 17:15:10 +0100

    feat: add card flip to display statistics

--- src/components/pages/Collection.jsx
+++ src/components/pages/Collection.jsx
@@ -1,6 +1,6 @@
 import './Collection.css';
 import React, { useState, useEffect } from 'react';
-import Tilt from 'react-parallax-tilt';
+import ReactCardFlip from 'react-card-flip';
 import Loading from '../loading/Loading';
 
 import { getUsercardsById } from '../../utils/connection';
@@ -8,6 +8,7 @@ import { getUsercardsById } from '../../utils/connection';
 const Profile = ({ theme, user }) => {
   const [loading, setLoading] = useState(true);
   const [usercards, setUsercards] = useState([]);
+  const [isFlipped, setIsFlipped] = useState({});
 
   useEffect(() => {
     const makeAsync = async () => {
@@ -20,6 +21,10 @@ const Profile = ({ theme, user }) => {
     makeAsync();
   }, [user.user_id]);
 
+  const handleClick = (id) => {
+    setIsFlipped({ ...isFlipped, [id]: !isFlipped[id] });
+  };
+
   if (loading) return <Loading theme={theme} />;
 
   return (
@@ -28,9 +33,24 @@ const Profile = ({ theme, user }) => {
         {usercards.length === 0 && <h1>YOUR COLLECTION IS EMPTY</h1>}
         {usercards.map((usercard) => {
           return (
-            <Tilt className={`collection-tilt`} key={Math.random()}>
-              <img className={`collection-card`} src={usercard.image_url} alt="card" />
-            </Tilt>
+            <ReactCardFlip
+              isFlipped={isFlipped[usercard.usercard_id]}
+              flipDirection="horizontal"
+              key={`${usercard.usercard_id}+${usercard.issue}`}
+            >
+              <button className={`collection-button`} onClick={() => handleClick(usercard.usercard_id)}>
+                <img className={`collection-card collection-card-${theme}`} src={usercard.image_url} alt="card" />
+              </button>
+              <button className={`collection-button`} onClick={() => handleClick(usercard.usercard_id)}>
+                <div className={`collection-card collection-card-${theme}`}>
+                  <p>{usercard.group}</p>
+                  <p>{usercard.name}</p>
+                  <p>#{usercard.issue}</p>
+                  <p>R{usercard.rank}</p>
+                  <p>X{usercard.xp}</p>
+                </div>
+              </button>
+            </ReactCardFlip>
           );
         })}
       </div>
