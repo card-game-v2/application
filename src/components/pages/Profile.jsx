e103fda7f2a82d6aade54df29d0865365f88a15a
Gareth Dawson <gwdawson.work@gmail.com>
2022-06-24 21:39:04 +0100

    style: add profile title

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -13,16 +13,14 @@ const Profile = ({ theme, user }) => {
 
   return (
     <section className={`profile profile-${theme}`}>
-      <div className={`profile-userinfo profile-userinfo-${theme}`}>
-        <img className={`profile-avatar`} src={`${user.avatar_url}`} alt="Profile Icon" />
+      <h1 className={`profile-title`}>PROFILE</h1>
+      <hr className={`profile-hr profile-hr-${theme}`} />
+      <div className={`profile-information`}>
+        <img src={user.avatar_url} alt="Profile Icon" />
         <div>
-          <h1 className={`profile-name`}>{`${user.username[0].toUpperCase()}${user.username.slice(1)}`}</h1>
-          <h4 className={`profile-id`}>{user.user_id}</h4>
+          <h1 className={`profile-username`}>{`${user.username[0].toUpperCase()}${user.username.slice(1)}`}</h1>
+          <h4 className={`profile-userid`}>{user.user_id}</h4>
         </div>
-      </div>
-      <div className={`profile-statistics profile-statistics-${theme}`}>
-        <h4>${user.currency}</h4>
-        <h4>{new Date(parseInt(user.join_date)).toLocaleDateString()}</h4>
       </div>
     </section>
   );
