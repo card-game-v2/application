7d77294bd434d2cfe53225e59e51107cf11c2efb
Gareth Dawson <gwdawson.work@gmail.com>
2022-06-25 16:18:15 +0100

    style: update profile styling

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -16,12 +16,21 @@ const Profile = ({ theme, user }) => {
       <h1 className={`profile-title`}>PROFILE</h1>
       <hr className={`profile-hr profile-hr-${theme}`} />
       <div className={`profile-information`}>
-        <img src={user.avatar_url} alt="Profile Icon" />
-        <div>
-          <h1 className={`profile-username`}>{`${user.username[0].toUpperCase()}${user.username.slice(1)}`}</h1>
-          <h4 className={`profile-userid`}>{user.user_id}</h4>
+        <div className={`profile-userinfo`}>
+          <img className={`profile-avatar`} src={user.avatar_url} alt="Profile Icon" />
+          <div>
+            <h1 className={`profile-username`}>{`${user.username[0].toUpperCase()}${user.username.slice(1)}`}</h1>
+            <h4 className={`profile-userid`}>{user.user_id}</h4>
+          </div>
+        </div>
+        <div className={`profile-statistics`}>
+          <p style={{ margin: '0px 0px 10px 0px' }}>Joined: 00/00/00</p>
+          <p style={{ margin: '0px 0px 10px 0px' }}>Cards: 1000</p>
+          <p style={{ margin: '0px 0px 10px 0px' }}>Trades: 1000</p>
+          <p style={{ margin: '0px 0px 00px 0px' }}>Achievements: 00/00</p>
         </div>
       </div>
+      <hr className={`profile-hr profile-hr-${theme}`} />
     </section>
   );
 };
